d7387475e290dead36677403387e972b66768b99
Pietro Pacini <pietrot96@gmail.com>
2026-01-06 18:34:25 +0100

    Created text formatter v0

---
+++ script.js
@@ -0,0 +1,10 @@
+function toLower() {
+    let text = "Hello World";
+    console.log(text.toLowerCase());
+}
+
+function toUpper() {
+    let text = "Hello World";
+    console.log(text.toUpperCase());
+}
+
