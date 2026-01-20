8913f13de4e1512744ad8ac8fbdc24d207352b52
Pietro Pacini <pietrot96@gmail.com>
2026-01-20 11:06:56 +0100

    Added interactivity and style

--- script.js
+++ script.js
@@ -1,10 +1,15 @@
-function toLower() {
-    let text = "Hello World";
-    console.log(text.toLowerCase());
+const lowerBtn = document.getElementById("lowercase-btn");
+const upperBtn = document.getElementById("uppercase-btn");
+const result = document.getElementById("result");
+const inputArea = document.getElementById("input");
+
+function getLowercase(text) {
+    result.textContent = text.toLowerCase();
 }
 
-function toUpper() {
-    let text = "Hello World";
-    console.log(text.toUpperCase());
+function getUppercase(text) {
+    result.textContent = text.toUpperCase();
 }
 
+lowerBtn.addEventListener("click", () => getLowercase(inputArea.value));
+upperBtn.addEventListener("click", () => getUppercase(inputArea.value));
