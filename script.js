4396c02fe4c76dcba76a44e72ed63df8cc002f9e
Pietro Pacini <pietrot96@gmail.com>
2026-01-20 12:10:46 +0100

    Added capitalise word function

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 const lowerBtn = document.getElementById("lowercase-btn");
 const upperBtn = document.getElementById("uppercase-btn");
+const capitaliseBtn = document.getElementById("capitalise-btn");
 const result = document.getElementById("result");
 const inputArea = document.getElementById("input");
 
@@ -11,5 +12,14 @@ function getUppercase(text) {
     result.textContent = text.toUpperCase();
 }
 
+function getCapitalised(text) {
+    let array = text.split(" ");
+    let allLower = array.map((word) => word.toLowerCase());
+    let capitalArray = allLower.map((word) => word.replace(word[0], word[0].toUpperCase()));
+    let final = capitalArray.join(" ");
+    result.textContent = final;
+}
+
 lowerBtn.addEventListener("click", () => getLowercase(inputArea.value));
 upperBtn.addEventListener("click", () => getUppercase(inputArea.value));
+capitaliseBtn.addEventListener("click", () => getCapitalised(inputArea.value));
