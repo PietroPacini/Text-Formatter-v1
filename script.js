5b5988a22ef6107b18e121ae2b1c10df60b7aa07
Pietro Pacini <pietrot96@gmail.com>
2026-02-01 22:13:33 +0100

    Updated getCapitalised function

--- script.js
+++ script.js
@@ -12,7 +12,7 @@ function getUppercase(text) {
 
 //Create an array of words, capitalise each word, then join in a string.
 function getCapitalised(text) {
-    const final = text.toLowerCase().split(" ").map((word) => {
+    const final = text.toLowerCase().split(" ").filter((word) => word).map((word) => {
         return word[0].toUpperCase() + word.slice(1);
     }).join(" ");
     result.textContent = final;
